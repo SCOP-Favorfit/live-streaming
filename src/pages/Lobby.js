55be012d6a45e55857070c8e58e0fee36a4a2a64
Asung <a.city521@gmail.com>
2022-12-17 15:18:11 +0900

    chore: initialize project

---
+++ src/pages/Lobby.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ConnectLive from "@connectlive/connectlive-web-sdk";
+
+function Lobby() {
+  return (
+      <div>
+        Lobby
+      </div>
+  );
+}
+
+export default Lobby;
