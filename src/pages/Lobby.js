ff137049f6333d7b78cf0fb7390299c66a4c3a99
Asung <a.city521@gmail.com>
2022-12-17 15:28:24 +0900

    feat: style of lobby

--- src/pages/Lobby.js
+++ src/pages/Lobby.js
@@ -1,10 +1,25 @@
 import React from "react";
 import ConnectLive from "@connectlive/connectlive-web-sdk";
+import "./style.css";
 
 function Lobby() {
   return (
-      <div>
-        Lobby
+      <div className="container">
+        <header>
+          <h1>Live Streaming</h1>
+        </header>
+        <main>
+          <section className="connect-room">
+            <form>
+              <input placeholder="Room ID" />
+              <button>Create Room</button>
+            </form>
+            <form>
+              <input placeholder="Room ID" />
+              <button>Join Room</button>
+            </form>
+          </section>
+        </main>
       </div>
   );
 }
