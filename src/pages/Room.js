d1569ec9e60b0f219700ad21039db41bb6352bff
Asung <a.city521@gmail.com>
2022-12-19 11:24:10 +0900

    feat: publish local video

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useRecoilState} from "recoil";
-import {RoomIdState} from "store/roomState";
+import {RoomIdState, RoomState} from "store/roomState";
+import ConnectLive from "@connectlive/connectlive-web-sdk";
+import {HostState} from "store/hostState";
 
 function Room() {
   const [roomId,] = useRecoilState(RoomIdState);
+  const [room,] = useRecoilState(RoomState);
+  const [isHost,] = useRecoilState(HostState);
+
+  useEffect(() => {
+    init();
+  }, [room])
+
+  const init = async() => {
+    const _room = room;
+    if (isHost) {
+      const _localMedia = await ConnectLive.createLocalMedia({
+        video: true,
+        audio: true,
+      });
+
+      const localVideo = _localMedia.video?.attach();
+      const localContainer = document.querySelector('#local-container');
+      localContainer.textContent = '';
+      localContainer.appendChild(localVideo)
+      await room.publish([_localMedia]);
+
+      room.on('participantEntered', (evt) => {
+        console.log(`## ${evt.remoteParticipant.id} joined`);
+      });
+      room.on('participantLeft', (evt) => {
+        console.log(`## ${evt.remoteParticipantId} left`);
+      });
+    } // end of host event
+    else {
+
+    } // end of guest event
+  }
 
   return (
       <div className="container">
@@ -12,7 +46,10 @@ function Room() {
         </header>
         <main>
           <div className="room-content">
-            <section>Media</section>
+            <section>
+              <div id="local-container"></div>
+              <div id="remote-container"></div>
+            </section>
             <section>Chat</section>
           </div>
         </main>
